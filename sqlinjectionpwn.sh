453a0e08e7e5c1105a128f47f316693a46785208
Black Hat Ethical Hacking <chris@blackhatethicalhacking.com>
2023-02-17 14:22:02 +0200

    Update sqlinjectionpwn.sh

--- sqlinjectionpwn.sh
+++ sqlinjectionpwn.sh
@@ -1,8 +1,24 @@
 figlet "SQL INJECT WITH 1337 STYLE" | lolcat
 sleep 0.5
 toilet -f mono12 -F metal by SaintDruG 
+echo "Enter Target URL with Parameter:" | lolcat
+read URL
+sqlmap -u $URL --risk=3 --level=5 --dbs --random-agent --batch
+echo "Injection Complete GL Bro, Try Harder!" | lolcat
+sleep 0.5
+echo "COPYRIGHTS ALL RIGHTS RESERVED - BLACK HAT ETHICAL HACKING" | lolcat
+sleep 0.5
+echo "---------------------------------------------------------------------" | lolcat
+sleep 0.5
+echo "Written by SaintDruG for Bug Bounty Purposes only for hackerone.com" | lolcat
+xcowsay blackhatethicalhacking.com
+# Description:
+#Written by SaintDruG 
+# This tool will start SQLMAP with Risk 3, Level 5 look for dbs using random agent without any 
+#evasion techniques for quick scan and identification of issues, it will automatcially answer all 
+#questions with Yes so it can go fast like lighting
 # Matrix effect
-echo "Entering the Matrix for 5 seconds in:" | toilet --metal -f term -F border
+echo "Exiting the Matrix for 5 seconds in:" | toilet --metal -f term -F border
 sleep 1
 echo "3" | toilet --gay -f term -F border
 sleep 1
@@ -29,19 +45,4 @@ for ((i=0; i<5; i++)); do
     echo -ne "${R}00 ${G}11 ${Y}01 ${B}10 ${P}11 ${C}00 ${W}10 ${G}01 ${P}11 ${B}00 ${Y}01 ${C}10\r"
     sleep 0.2
 done
-echo "Enter Target URL with Parameter:" | lolcat
-read URL
-sqlmap -u $URL --risk=3 --level=5 --dbs --random-agent --batch
-echo "Injection Complete GL Bro, Try Harder!" | lolcat
-sleep 0.5
-echo "COPYRIGHTS ALL RIGHTS RESERVED - BLACK HAT ETHICAL HACKING" | lolcat
-sleep 0.5
-echo "---------------------------------------------------------------------" | lolcat
-sleep 0.5
-echo "Written by SaintDruG for Bug Bounty Purposes only for hackerone.com" | lolcat
-xcowsay blackhatethicalhacking.com
-# Description:
-#Written by SaintDruG 
-# This tool will start SQLMAP with Risk 3, Level 5 look for dbs using random agent without any 
-#evasion techniques for quick scan and identification of issues, it will automatcially answer all 
-#questions with Yes so it can go fast like lighting
+echo -e "\033[2J\033[?25h" # reset screen
